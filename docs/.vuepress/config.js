f12249fcfd52bceaac92e9fd4606dc8205d98562
sunhongpeng <sunhongpeng0817@163.com>
2020-06-25 00:36:09 +0800

    ‘配置修改’

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,93 +1,94 @@
 module.exports = {
-    title: '前端开发规范初稿', 
+    title: '前端开发规范初稿',
     description: '前端开发规范初稿',
     base: '/webnorm/', // 将部署站点的基本URL
-    head: [
-      
-    ],
+    head: [],
     DEST: '.vuepress/dist', // 指定输出目录
-    themeConfig:{
+    themeConfig: {
         lastUpdated: '上次更新',
-		serviceWorker: {
-			updatePopup: {
-				message: '发现新内容可用',
-				buttonText: '刷新'
-			}
-		},
-		displayAllHeaders: true, // 展开所有标题
-		editLinks: true,
-        nav:[
+        serviceWorker: {
+            updatePopup: {
+                message: '发现新内容可用',
+                buttonText: '刷新'
+            }
+        },
+        displayAllHeaders: true, // 展开所有标题
+        editLinks: true,
+        nav: [
             {
-                text:"快速入门",
-                link:"/"
+                text: "快速入门",
+                link: "/"
             },
             {
-                text:"项目规范",
-                link:"/name/"
+                text: "项目规范",
+                link: "/name/"
             },
             {
-				text: '代码规范',
-				items: [
-					{
-						text: 'HTML',
-						link: '/html/'
-					},
-					{
-						text: 'CSS',
-						link: '/css/'
-					},
-					{
-						text: 'JavaScript',
-						link: '/javascript/'
-					},
-					{
-						text: 'ECMAScript 6',
-						link: '/es6/'
-					}
-				]
-			},
-		],
-		sidebar: {
-			// 命名规范
-			'/name/': [
-				{
-					title: '',
-					collapsable: false,
-					children: [ '/name/' ]
-				}
-			],
-			// 代码规范 html
-			'/html/': [
-				{
-					title: '',
-					collapsable: false,
-					children: [ '/html/' ]
-				}
-			],
-			// 代码规范 css
-			'/css/': [
-				{
-					title: '',
-					collapsable: false,
-					children: [ '/css/' ]
-				}
-			],
-			// 代码规范 javascript
-			'/javascript/': [
-				{
-					title: '',
-					collapsable: false,
-					children: [ '/javascript/' ]
-				}
-			],
-			// 代码规范 es6
-			'/ES6/': [
-				{
-					title: '',
-					collapsable: false,
-					children: [ '/ES6/' ]
-				}
-			],
-	}
-}
-}
+                text: '代码规范',
+                items: [
+                    {
+                        text: 'HTML',
+                        link: '/html/'
+                    },
+                    {
+                        text: 'CSS',
+                        link: '/css/'
+                    },
+                    {
+                        text: 'JavaScript',
+                        link: '/javascript/'
+                    },
+                    {
+                        text: 'ECMAScript 6',
+                        link: '/es6/'
+                    }
+                ]
+            },
+        ],
+        sidebar: {
+            // 命名规范
+            '/name/': [
+                {
+                    title: '',
+                    collapsable: false,
+                    children: ['/name/']
+                }
+            ],
+            // 代码规范 html
+            '/html/': [
+                {
+                    title: '',
+                    collapsable: false,
+                    children: ['/html/']
+                }
+            ],
+            // 代码规范 css
+            '/css/': [
+                {
+                    title: '',
+                    collapsable: false,
+                    children: ['/css/']
+                }
+            ],
+            // 代码规范 javascript
+            '/javascript/': [
+                {
+                    title: '',
+                    collapsable: false,
+                    children: ['/javascript/']
+                }
+            ],
+            // 代码规范 es6
+            '/ES6/': [
+                {
+                    title: '',
+                    collapsable: false,
+                    children: ['/ES6/']
+                }
+            ],
+        }
+    },
+    Markdown: {
+        lineNumbers: true,// 是否在每个代码块的左侧显示行号。
+    }
+};
--- docs/README.md
+++ docs/README.md
@@ -4,5 +4,16 @@ next: ./name/
 editLink: true
 ---
 
-# 快速入门
+# 快速开始
 
+`克隆工程`或`下载工程`到本地
+
+### yarn install
+
+工程包管理工具依赖`yarn`,请先执行 `yarn install`，没有按照`yarn`的请下全局安装一下。
+
+### yarn run docs:build
+
+vuepress 本地构建，打包
+
+### yarn run docs:dev
